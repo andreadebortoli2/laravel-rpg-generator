e6ed13b44a621950fb59f26c733592ac13a7855d
simoneevacca <v.simone.vacca@gmail.com>
2024-05-14 16:56:24 +0200

    create PageController and model close#3

---
+++ resources/views/guests/item.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('page-title', 'home')
+
+@section('content')
+    <div>item content</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Guests\PageController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guests.home');
-});
+
+
+Route::get('/', [PageController::class, 'index'])->name('guests.home');
+
+Route::get('/item', [PageController::class, 'show'])->name('guests.item');
