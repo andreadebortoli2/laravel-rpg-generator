8f720d284e729300fe6c44d003b51dcdf5352b88
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-17 16:27:56 +0200

    add header close #5

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,11 @@
-<header>header</header>
+<header class="bg-success-subtle">
+    <nav class="container navbar navbar-expand navbar-light">
+        <div class="nav navbar-nav">
+            <a class="nav-item nav-link {{ Route::currentRouteName() === 'guests.home' ? 'active' : '' }}"
+                href="{{ route('guests.home') }}">Home</a>
+            <a class="nav-item nav-link {{ Route::currentRouteName() === 'items.index' ? 'active' : '' }}"
+                href="{{ route('items.index') }}">Items</a>
+        </div>
+    </nav>
+
+</header>
