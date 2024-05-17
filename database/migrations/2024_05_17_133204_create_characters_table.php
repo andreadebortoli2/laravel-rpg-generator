872e9d206c2e0ecdc745eab255d2c55b57387f1e
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-17 15:39:48 +0200

    add character model, migration

---
+++ database/migrations/2024_05_17_133204_create_characters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->unique();
+            $table->text('description')->nullable();
+            $table->string('attack', 10)->nullable();
+            $table->string('defense', 10)->nullable();
+            $table->string('speed', 10)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('characters');
+    }
+};
