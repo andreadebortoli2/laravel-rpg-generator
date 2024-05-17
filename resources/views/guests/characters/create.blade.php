19f7249a711641846345843f341f9ce85ed37c6e
Nico-Serra <nico.serra000@gmail.com>
2024-05-17 16:32:22 +0200

    pre mergecommit

---
+++ resources/views/guests/characters/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('page-title', 'Create')
+
+@section('content')
+
+
+<div class="container">
+    <form action="{{route('characters.store')}}" method="post">
+    
+        
+    
+    </form>
+</div>
+    
+@endsection
